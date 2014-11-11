01f75bb5a17f18d5056e8d027be854adf3fad07b
Sammydude <samschroyen@gmail.com>
2014-11-11 21:08:10 +0100

    Project created

---
+++ src/sams/defence/View.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sams.defence;
+import java.awt.*;
+/**
+ *
+ * @author Sam
+ */
+public class View extends Canvas {
+    
+}
