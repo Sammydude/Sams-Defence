5c89f9c554b3498a5c9331e9d9bf9a9952f89fab
Sammydude <samschroyen@gmail.com>
2014-11-11 22:49:51 +0100

    First classes made

--- src/sams/defence/SamsDefence.java
+++ src/sams/defence/SamsDefence.java
@@ -4,18 +4,32 @@
  * and open the template in the editor.
  */
 package sams.defence;
-
+import java.awt.*;
 /**
  *
  * @author Sam
  */
-public class SamsDefence {
+public class SamsDefence extends Frame {
 
+    private Level model;
+    private View view;
+    private Controller controller;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        SamsDefence venster = new SamsDefence();
+        venster.setTitle("Sams Defence");
+        venster.setSize(600, 480);
+        venster.setVisible(true);
     }
     
+    public SamsDefence() {
+        //initialiseren van datamembers
+        model = new Level();
+        view = new View(model);
+        add(view);
+        controller = new Controller(model,view);
+    }
 }
